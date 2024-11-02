87bb6c4df971aace7f97be1eba2597a4e684d11c
kb <kb@local.host>
2024-11-02 18:19:25 +0100

    Formularz Logowania i rejestracji

---
+++ app/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+
+app.get('/',(req,res) => {
+    res.render('index');
+});
+
+app.get('/login',(req,res) => {
+    res.render('login');
+});
+
+app.get('/register',(req,res) => {
+    res.render('register');
+});
+app.get('/dashboard',(req,res)=>{
+    res.render('dashboard');
+});
+
+
+
+
+app.listen(PORT,()=>{
+    console.log('dzialo..');
+});
